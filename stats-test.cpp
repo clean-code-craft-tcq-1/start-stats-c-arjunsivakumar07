23d33c680f0839451b017fc1c4b4db8b38290093
arjunsivakumar07 <Arjun@714>
2021-01-28 08:10:54 +0530

    Adding header files

--- stats-test.cpp
+++ stats-test.cpp
@@ -1,8 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "catch.hpp"
-#include "stats.c"
-#include "Alerter.c"
+#include "stats.h"
+#include "Alerter.h"
 
 #include <stdlib.h>
 #include <math.h>
